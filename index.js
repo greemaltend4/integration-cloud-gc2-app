5338fea4bf1a5d4a05a0bcaf7237964431c65d3d
greemaltend4 <dengestabiy@hotmail.com>
2025-11-04 11:07:07 +0000

    feat: initial commit for index.js

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('Bienvenue dans lIntegration Cloud GC2 App');
+});
+
+app.listen(PORT, () => {
+  console.log(`L'application écoute sur le port ${PORT}`);
+});
